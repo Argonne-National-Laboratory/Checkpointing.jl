68835b6d4bcfdf0c5739e1741b30be974e59a571
sriharikrishna <sriharikrishna@gmail.com>
2021-11-19 14:56:58 -0600

    Call periodic checkpointing

--- src/Checkpointing.jl
+++ src/Checkpointing.jl
@@ -7,8 +7,8 @@ export mynorm
 
 abstract type Scheme end
 
-include("Revolve/Revolve.jl")
-#include("Revolve/Periodic.jl")
+#include("Revolve/Revolve.jl")
+include("Revolve/Periodic.jl")
 
 export Revolve, guess, factor, next_action!, ActionFlag
 
