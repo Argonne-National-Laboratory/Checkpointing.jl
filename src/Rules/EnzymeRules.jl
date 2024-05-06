964d28c44d9ce039646eeba32d49a9e45651073e
Michel Schanen <mschanen@anl.gov>
2024-05-06 14:12:29 -0500

    Fix primal again

--- src/Rules/EnzymeRules.jl
+++ src/Rules/EnzymeRules.jl
@@ -11,11 +11,12 @@ function augmented_primal(
     model,
     range,
 )
-    primal = func.val(body.val, alg.val, deepcopy(model.val), range.val)
+    tape_model = deepcopy(model.val)
+    func.val(body.val, alg.val, model.val, range.val)
     if needs_primal(config)
-        return AugmentedReturn(primal, nothing, (model.val,))
+        return AugmentedReturn(nothing, nothing, (tape_model,))
     else
-        return AugmentedReturn(nothing, nothing, (model.val,))
+        return AugmentedReturn(nothing, nothing, (tape_model,))
     end
 end
 
@@ -30,14 +31,13 @@ function reverse(
     range,
 )
     (model_input,) = tape
-    model_final = Checkpointing.rev_checkpoint_struct_for(
+    Checkpointing.rev_checkpoint_struct_for(
         body.val,
         alg.val,
         model_input,
         model.dval,
         range.val,
     )
-    copyto!(model.val, model_final)
     return (nothing, nothing, nothing, nothing)
 end
 
@@ -50,11 +50,12 @@ function augmented_primal(
     model,
     condition,
 )
-    primal = func.val(body.val, alg.val, deepcopy(model.val), condition.val)
+    tape_model = deepcopy(model.val)
+    func.val(body.val, alg.val, model.val, condition.val)
     if needs_primal(config)
-        return AugmentedReturn(primal, nothing, (model.val,))
+        return AugmentedReturn(nothing, nothing, (tape_model,))
     else
-        return AugmentedReturn(nothing, nothing, (model.val,))
+        return AugmentedReturn(nothing, nothing, (tape_model,))
     end
 end
 
@@ -69,14 +70,13 @@ function reverse(
     condition,
 )
     (model_input,) = tape
-    model_final = Checkpointing.rev_checkpoint_struct_while(
+    Checkpointing.rev_checkpoint_struct_while(
         body.val,
         alg.val,
         model_input,
         model.dval,
         condition.val,
     )
-    copyto!(model.val, model_final)
     return (nothing, nothing, nothing, nothing)
 end
 
