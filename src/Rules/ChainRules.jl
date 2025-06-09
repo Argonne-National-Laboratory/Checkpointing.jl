520360209a6a4deaafae97d287eb5566dc9d5512
Michel Schanen <Michel.Schanen@gmail.com>
2025-06-09 10:42:31 -0500

    Fix RevConfig error

--- src/Rules/ChainRules.jl
+++ src/Rules/ChainRules.jl
@@ -18,7 +18,7 @@ function ChainRulesCore.rrule(
         set_zero!(shadowmodel)
         copyto!(shadowmodel, dmodel)
         # mock an RevConfig object
-        config = EnzymeCore.EnzymeRules.RevConfig{false, true, 1, false, false}()
+        config = EnzymeCore.EnzymeRules.RevConfig{false, true, 1, false, false, false}()
         model = rev_checkpoint_struct_for(config, body, alg, model_input, shadowmodel, range)
         dshadowmodel = create_tangent(shadowmodel)
         return NoTangent(), NoTangent(), NoTangent(), dshadowmodel, NoTangent(), NoTangent()
@@ -42,7 +42,7 @@ function ChainRulesCore.rrule(
         set_zero!(shadowmodel)
         copyto!(shadowmodel, dmodel)
         # mock an RevConfig object
-        config = EnzymeCore.EnzymeRules.RevConfig{false, true, 1, false, false}()
+        config = EnzymeCore.EnzymeRules.RevConfig{false, true, 1, false, false, false}()
         model = rev_checkpoint_struct_while(config, body, alg, model_input, shadowmodel, condition)
         dshadowmodel = create_tangent(shadowmodel)
         return NoTangent(), NoTangent(), NoTangent(), dshadowmodel, NoTangent(), NoTangent()
